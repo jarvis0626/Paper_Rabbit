59e38cf237d9a01607dae840c99bfdff77d84a10
Jarvis Suvi <jarvissuvi@gmail.com>
2026-07-09 14:50:41 +0530

    Phase 0-Basic backend implementation

---
+++ backend/src/test/java/com/paperrabbit/backend/PaperRabbitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paperrabbit.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperRabbitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
